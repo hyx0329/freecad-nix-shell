dce619246c5aacc59476c00780c8d6c22dc0fe2d
hyx0329 <hyx0329@163.com>
2024-06-08 17:22:55 +0800

    update to new api, add hisa libs to LD_LIBRARY_PATH

--- freecad-cfd.nix
+++ freecad-cfd.nix
@@ -37,7 +37,7 @@ let
 in
 
 pkgs.mkShell {
-  buildInputs = [
+  packages = [
     pkgs.python3
     pkgs.git
     pkgs.curl
@@ -57,8 +57,15 @@ pkgs.mkShell {
     hisa
   ];
 
+  # What the hook does:
+  # add nixGL dynamic libs to LD_LIBRARY_PATH
+  # add hisa dynamic libs to LD_LIBRARY_PATH
+  # set FreeCAD storage locations
+  # some shorthands for shell access
+  # load OpenFOAM environment
   shellHook = ''
     export LD_LIBRARY_PATH=$(nixGLIntel printenv LD_LIBRARY_PATH):$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${hisa}/lib:$LD_LIBRARY_PATH
     export FREECAD_USER_HOME=$(pwd)/freecad-state/home
     export FREECAD_USER_DATA=$(pwd)/freecad-state/userdata
     export FREECAD_USER_TEMP=$(pwd)/freecad-state/temp
